70b353541e5fa0b4b90b2fa3f23df41ef868865d
YuShaoSu <n270882@gmail.com>
2025-04-20 17:57:21 +0800

    Try fix build fail

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,20 @@
+// app/layout.tsx
 import '../styles/globals.css';
+import type { Metadata } from 'next';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'TYDS 天羽設計',
   description: '現代極簡室內設計作品集網站',
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="zh-Hant">
+      <head />
       <body>{children}</body>
     </html>
   );
